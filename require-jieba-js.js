84f826544870adacf74276aacfb6a29c6376673a
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-03-17 17:57:22 +0800

    0317 endsWith

--- require-jieba-js.js
+++ require-jieba-js.js
@@ -48,25 +48,28 @@ resume_jieba_cut = function () {
 
 // ------------------------------------------------------
 
-if (!String.prototype.endsWith) {
-  String.prototype.endsWith = function(searchString, position) {
-      var subjectString = this.toString();
-      if (typeof position !== 'number' 
-		|| !isFinite(position) 
-		|| Math.floor(position) !== position 
-		|| position > subjectString.length) {
-        position = subjectString.length;
-      }
-      position -= searchString.length;
-      var lastIndex = subjectString.lastIndexOf(searchString, position);
-      return lastIndex !== -1 && lastIndex === position;
-  };
-}
+
 
 
 // ------------------------------------------------------
 
 _get_host = function () {
+	
+	if (!String.prototype.endsWith) {
+	  String.prototype.endsWith = function(searchString, position) {
+		  var subjectString = this.toString();
+		  if (typeof position !== 'number' 
+			|| !isFinite(position) 
+			|| Math.floor(position) !== position 
+			|| position > subjectString.length) {
+			position = subjectString.length;
+		  }
+		  position -= searchString.length;
+		  var lastIndex = subjectString.lastIndexOf(searchString, position);
+		  return lastIndex !== -1 && lastIndex === position;
+	  };
+	}
+	
 	var _host = "";
 	var _scripts = $("script");
 	for (var _i = 0; _i < _scripts.length; _i++) {
