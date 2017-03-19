810618e0167534a7579698a5cce351b7d595736c
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-03-19 23:41:30 +0800

    0319 改完忘記推了

--- codepen/text-mining-arff2csv.js
+++ codepen/text-mining-arff2csv.js
@@ -1,56 +1,118 @@
+
 var _process_file = function(_input, _buffer, _callback) {
     _loading_enable();
    
-   var _pro_dis = [];
-   var _pro_dis_attr = [];
+   //var _old_weka = true;
+   
    var _predicted_data = [];
+   
+   var _pro_dis = [];
+   var _pro_dis_attr = ["籃球", "地球", "撞球"];
+   
+   var _line_process_plain_text = function (_line) {
+		var _pos = _line.lastIndexOf("+");
+		if (_pos === -1) {
+			_pos = _line.lastIndexOf("-");
+		}
+
+		var _pd = _line.substring(_pos + 1, _line.length).trim();
+		while (_pd.indexOf("  ") > -1) {
+			 _pd = _pd.replace(/  /g, ' ');
+		}
+		
+		if (_line.indexOf("*") > -1) {
+			//_old_weka = false;
+			_pd = _pd.replace(/\*/g, '');
+			var _fields = _pd.split(" ");
+			_pro_dis.push(_fields);
+		}
+		
+		// ---------------------
+		var _pos = _line.lastIndexOf(":");
+		var _pos2 = _line.indexOf(" ", _pos);
+		var _pre = _line.substring(_pos+1, _pos2);
+		_predicted_data.push(_pre);
+		
+		// ----------------------
+		
+		if (_line.indexOf("*") > -1) {
+			var _pos = _line.lastIndexOf(":");
+			var _len = 1;
+			while (_line.substring(_pos-_len, _pos- _len + 1) !== " ") {
+				_len++;
+			}
+			var _index = _line.substring(_pos-_len, _pos);
+			_index = parseInt(_index, 10) - 1;
+			_pro_dis_attr[_index] = _pre;
+			console.log(_index);
+		}
+		else {
+			var _pos = _line.lastIndexOf(" ", _line.length-2);
+			var _index = _line.substring(_pos+1, _line.length).trim();
+			_index = parseFloat(_index, 10);
+			_pro_dis.push(_index);
+		}
+   };	//var _line_process_plain_text = function (_line) {
+	   
+	var _line_process_csv = function (_line, _line_number) {
+		console.log(_line);
+		var _fields = _line.split(",");
+		
+		
+		//if (_line.indexOf("*") > -1) {
+		//	_pro_dis.push(_fields);
+		//}
+		
+		// ---------------------
+		var _pos = _line.lastIndexOf(":");
+		var _pos2 = _line.indexOf(",", _pos);
+		var _pre = _line.substring(_pos+1, _pos2);
+		_predicted_data.push(_pre);
+		
+		// ----------------------
+		
+		if (_line.indexOf("*") > -1) {
+			_line_number--;
+			_pro_dis[_line_number] = [];
+			for (var _i = 4; _i < _fields.length; _i++) {
+				var _index = _fields[_i];
+				_pro_dis[_line_number].push(_index);
+			}
+		}
+		else {
+			var _pos = _line.lastIndexOf(",", _line.length-2);
+			var _index = _line.substring(_pos+1, _line.length).trim();
+			_index = parseFloat(_index, 10);
+			_pro_dis.push(_index);
+		}
+   };	//var _line_process_plain_text = function (_line) {
    
    //var _buffer = $("#input_mode_textarea_buffer").val().trim();
    if (_buffer !== undefined) {
         var _head_needle = "\n=== Predictions on test split ===\n";
         var _head_pos = _buffer.indexOf(_head_needle);
+		if (_head_pos === -1) {
+			_head_needle = "\n=== Predictions on test set ===\n";
+			_head_pos = _buffer.indexOf(_head_needle);
+		}
         var _footer_needle = "\n=== Evaluation on test set ===\n";
         var _footer_pos = _buffer.indexOf(_footer_needle, _head_pos + _head_needle.length);
         if (_head_pos !== -1 && _footer_pos !== -1) {
             _buffer = _buffer.substring(_head_pos + _head_needle.length, _footer_pos).trim();
             var _lines = _buffer.split("\n");
             for (var _l = 1; _l < _lines.length; _l++)  {
-                var _line = _lines[_l];
-                var _pos = _line.lastIndexOf("+");
-                if (_pos === -1) {
-                    _pos = _line.lastIndexOf("-");
-                }
-
-                var _pd = _line.substring(_pos + 1, _line.length).trim();
-                while (_pd.indexOf("  ") > -1) {
-                     _pd = _pd.replace(/  /g, ' ');
-                }
-                _pd = _pd.replace(/\*/g, '');
-                var _fields = _pd.split(" ");
-                _pro_dis.push(_fields);
-                
-                // ---------------------
-                var _pos = _line.lastIndexOf(":");
-                var _pos2 = _line.indexOf(" ", _pos);
-                var _pre = _line.substring(_pos+1, _pos2);
-                _predicted_data.push(_pre);
-                
-                // ----------------------
-                
-                 var _pos = _line.lastIndexOf(":");
-                 var _len = 1;
-                 while (_line.substring(_pos-_len, _pos- _len + 1) !== " ") {
-                     _len++;
-                 }
-                 var _index = _line.substring(_pos-_len, _pos);
-                 console.log(_index);
-                 _index = parseInt(_index, 10) - 1;
-                 _pro_dis_attr[_index] = _pre;
-            }
+				var _line = _lines[_l];
+				if (_line.indexOf(",") === -1) {
+					_line_process_plain_text(_line);
+				}	// if (_line.indexOf(",") === -1) {
+				else {
+					_line_process_csv(_line, _l);
+				}
+			}
         }
    }
     
-   
+   //console.log(_pro_dis);
 
   //------------------
 
@@ -83,14 +145,20 @@ var _process_file = function(_input, _buffer, _callback) {
           _attr_list.push("predictedclass");
       }
       
-      for (var _i = 0; _i < _pro_dis[0].length; _i++) {
-          var _attr = "probability distribution " + (_i+1);
-          if (typeof(_pro_dis_attr[_i]) === "string") {
-              _attr = "probability distribution: " + _pro_dis_attr[_i];
-          }
-          
-          _attr_list.push(_attr);
-      }
+	  if (typeof(_pro_dis[0]) === "object") {
+		  for (var _i = 0; _i < _pro_dis[0].length; _i++) {
+			  var _attr = "pro_dis: " + (_i+1);
+			  if (typeof(_pro_dis_attr[_i]) === "string") {
+				  _attr = "pro_dis: " + _pro_dis_attr[_i];
+			  }
+			  _attr_list.push(_attr);
+		  }
+	  }
+	  else {
+		  console.log(121);
+		  var _attr = "probability distribution";
+		  _attr_list.push(_attr);
+	  }
   }
   // --------------------------------------
   
@@ -117,7 +185,7 @@ var _process_file = function(_input, _buffer, _callback) {
             //    _predict = _value;
             //}
         }
-        if (typeof(_pro_dis[_l]) !== "undefined") {
+        if (typeof(_pro_dis[_l]) === "object") {
             if (_has_predicted === false) {
                 _temp_line.push(_predicted_data[_l]);
             }
@@ -139,6 +207,18 @@ var _process_file = function(_input, _buffer, _callback) {
             //_pro_dis_attr[_max_index] = _predict;
             //console.log([_max_index, _predict]);
         }
+		else {
+			if (_has_predicted === false && typeof(_predicted_data[_l]) !== "undefined") {
+                _temp_line.push(_predicted_data[_l]);
+            }
+            
+            // ---------------------------
+            
+			if (typeof(_pro_dis[_l]) !== "undefined") {
+				var _pd = _pro_dis[_l];
+				_temp_line.push(_pd);
+			}
+		}
         
         _temp_result.push(_temp_line.join(","));
     }
