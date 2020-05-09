eb069a7fe8bd7ec1ded12f11957137a8ba9e8d0e
Pulipuli Chen <blog@pulipuli.info>
2020-05-10 00:52:50 +0800

    0510 #5 Step 2 做好了

--- html-lib/vue-app.js
+++ html-lib/vue-app.js
@@ -1,8 +1,25 @@
 var app = new Vue({
   el: '#app',
   data: {
-    step: 0,
-    message: 'Hello Vue!'
+    step: 1,
+    segmentationMethod: 'dictionary',
+    nGramLength: 2,
+    configUserDictionary: `找尋樂趣,9999999,n
+無聊的世界,9999999,n`,
+    configUserDictionaryExample: '',
+    configWordRemap: `食物,甜點`,
+    configWordRemapExample: '',
+    configStopWords: `這個
+，
+、
+。`,
+    configStopWordsExample: '',
+    usePorterStemmer: true
+  },
+  mounted () {
+    this.configUserDictionaryExample = this.configUserDictionary
+    this.configWordRemapExample = this.configWordRemap
+    this.configStopWordsExample = this.configStopWords
   },
   methods: {
     computedStepClass: function (stepID) {
@@ -10,6 +27,42 @@ var app = new Vue({
         'completed': (this.step > stepID),
         'active': (this.step === stepID)
       }
+    },
+    downloadConfiguration () {
+      console.error('@TODO')
+    },
+    setExampleUserDictionary () {
+      if (this.configUserDictionaryExample !== this.configUserDictionary) {
+        if (window.confirm('Are you sure to replace existed content?') === false) {
+          return false
+        }
+      }
+      else {
+        return false
+      }
+      this.configUserDictionary = this.configUserDictionaryExample
+    },
+    setExampleWordRemap () {
+      if (this.configWordRemapExample !== this.configWordRemap) {
+        if (window.confirm('Are you sure to replace existed content?') === false) {
+          return false
+        }
+      }
+      else {
+        return false
+      }
+      this.configWordRemap = this.configWordRemapExample
+    },
+    setExampleStopWords () {
+      if (this.configStopWordsExample !== this.configStopWords) {
+        if (window.confirm('Are you sure to replace existed content?') === false) {
+          return false
+        }
+      }
+      else {
+        return false
+      }
+      this.configStopWords = this.configStopWordsExample
     }
   }
 })
