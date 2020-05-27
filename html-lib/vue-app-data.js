e8cb6d29da35441e69cb1227e18ba2963b2a15f2
Pulipuli Chen <blog@pulipuli.info>
2020-05-27 15:11:31 +0800

    0527 加入了預先載入停用字的做法

--- html-lib/vue-app-data.js
+++ html-lib/vue-app-data.js
@@ -1,34 +1,36 @@
 var appData = {
-    inputFilename: `Raw Text`,
-    inputText: `message
+  inputFilename: `Raw Text`,
+  inputText: `message
 這個<strong>布丁</strong> 是在無聊的世界中找尋code的1998種不能吃的codes，
 喜愛動漫畫、遊戲、Coding，以及ABCDV跟世間脫節的生活步調。`,
-    outputText: ``,
-    //step: 2,
-    segmentationMethod: 'dictionary',
-    //segmentationMethod: 'n-gram',
-    nGramLength: 2,
-    configUserDictionary: `找尋樂趣,9999999,n
+  outputText: ``,
+  //step: 2,
+  segmentationMethod: 'dictionary',
+  //segmentationMethod: 'n-gram',
+  nGramLength: 2,
+  configUserDictionary: `找尋樂趣,9999999,n
 無聊的世界,9999999,n`,
-    configUserDictionaryExample: '',
-    configWordRemap: `食物,甜點`,
-    configWordRemapExample: '',
-    configStopWords: `這個
+  configUserDictionaryExample: '',
+  configWordRemap: `食物,甜點`,
+  configWordRemapExample: '',
+  configStopWordsFile: `./stop_words.txt`,
+  //configStopWordsFile: null,  // 預設不載入
+  configStopWords: `這個
 ，
 、
 。`,
-    configStopWordsExample: '',
-    fullStopWords: null,
-    removeEnglish: false,
-    removeNumber: false,
-    removeHTML: true,
-    usePorterStemmer: true,
-    useLowerCase: true,
-    jiebaInited: false,
-    processOutputWait: false,
-    displayPanel: 'text',
-    //displayPanel: 'configuration',
-    persistKey: 'jieba-js.' + location.href,
-    configChanged: false,
-    doRemoveHeader: false,
-  }
+  configStopWordsExample: '',
+  fullStopWords: null,
+  removeEnglish: false,
+  removeNumber: false,
+  removeHTML: true,
+  usePorterStemmer: true,
+  useLowerCase: true,
+  jiebaInited: false,
+  processOutputWait: false,
+  displayPanel: 'text',
+  //displayPanel: 'configuration',
+  persistKey: 'jieba-js.' + location.href,
+  configChanged: false,
+  doRemoveHeader: false,
+}
--- html-lib/vue-app-mounted.js
+++ html-lib/vue-app-mounted.js
@@ -1,19 +1,22 @@
-var appMount = function () {
-    this.setupAPI()
-    
-    this.configUserDictionaryExample = this.configUserDictionary
-    this.configWordRemapExample = this.configWordRemap
-    this.configStopWordsExample = this.configStopWords
-    
-    //console.log(stemmer("hopefully", true), stemmer("loves", true))
-    
-    this.loadPersistedData()
-    postMessageAPI.ready()
-    
-    //console.log(this.searchParams.api)
-    if (!this.searchParams.api) {
-      setTimeout(() => {
-        this.processOutput()
-      }, 0)
-    }
+/* global postMessageAPI */
+
+var appMount = async function () {
+  this.setupAPI()
+
+  this.configUserDictionaryExample = this.configUserDictionary
+  this.configWordRemapExample = this.configWordRemap
+  this.configStopWordsExample = this.configStopWords
+
+  //console.log(stemmer("hopefully", true), stemmer("loves", true))
+
+  this.loadPersistedData()
+  postMessageAPI.ready()
+  await this.loadFullStopWordsOnMount()
+
+  //console.log(this.searchParams.api)
+  if (!this.searchParams.api) {
+    setTimeout(() => {
+      this.processOutput()
+    }, 0)
   }
+}
